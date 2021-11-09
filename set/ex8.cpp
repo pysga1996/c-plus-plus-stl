61ebb63dd08e20a0b3bba06f869d7fc519628e26
thanhvt <thanhvt@vissoft.vn>
2021-11-09 16:06:28 +0700

    add set/ex8, ex9

--- set/ex8.cpp
+++ set/ex8.cpp
@@ -1,4 +1,24 @@
+#include "main.h"
+
 namespace set_exercises {
 
-    void ex8() {};
+    int cyclicShift(string word);
+
+    void ex8() {
+        string input;
+        cout << "Enter a word: " << endl;
+        cin >> input;
+        cout << "Cyclic shifts count of the given word is: " << cyclicShift(input) << endl;
+    }
+
+    int cyclicShift(string word) {
+        set<string> s;
+        for (int i = 0; i <= word.size(); i++) {
+            char x = word.back();
+            word.pop_back();
+            word.insert(word.begin() + 0, x);
+            s.insert(word);
+        }
+        return s.size();
+    }
 }
--- set/ex9.cpp
+++ set/ex9.cpp
@@ -1,4 +1,36 @@
+#include "main.h"
+
 namespace set_exercises {
 
-    void ex9() {};
+    string diversity(string str, int k);
+
+    void ex9() {
+        string input;
+        int k;
+        cout << "Enter word and k: " << endl;
+        cin >> input >> k;
+        cout
+                << "Minimum characters need to change to make the given word being combination of different characters is: "
+                << diversity(input, k) << endl;
+    }
+
+    string diversity(string str, int k) {
+        set<char> s;
+        int i;
+        string ret;
+        if (str.size() < k) {
+            ret = "impossible";
+        } else {
+            for (i = 0; i < str.size(); i++) {
+                s.insert(str.at(i));
+            }
+            if (s.size() >= k) {
+                i = 0;
+            } else {
+                i = k - s.size();
+            }
+            ret = to_string(i);
+        }
+        return ret;
+    }
 }
